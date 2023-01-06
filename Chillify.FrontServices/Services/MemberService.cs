59197bbd14d1333dc5176af0ccf6c1ccd99b8a84
Philemon Philippin <p_philippin@hotmail.com>
2023-01-06 18:09:48 +0100

    Token dans le request header

--- Chillify.FrontServices/Services/MemberService.cs
+++ Chillify.FrontServices/Services/MemberService.cs
@@ -1,18 +1,28 @@
 ﻿
+using System.Net.Http.Headers;
+using System.Net.Http;
+
 namespace Chillify.FrontServices.Services;
 
 public class MemberService : IMemberService
 {
     private readonly HttpClient _http;
+    private readonly ILocalStorage _localStorage;
 
-    public MemberService(HttpClient http)
+    public MemberService(HttpClient http, ILocalStorage localStorage)
     {
         _http = http;
+        _localStorage = localStorage;
     }
 
     public async Task<ServiceResponse<List<Member>>> GetMembers()
     {
+        string token = await _localStorage.GetToken();
+
+        _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
         var response = await _http.GetFromJsonAsync<ServiceResponse<List<Member>>>("api/member/get");
+
         if (response is null)
         {
             return new ServiceResponse<List<Member>>()
